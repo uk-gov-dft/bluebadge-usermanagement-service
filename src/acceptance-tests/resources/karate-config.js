66b7fb8bcf25306adcef0e52e5337dd60cafd6c7
Stephen Elliott <stephen@bealine.co.uk>
2018-07-17 14:54:22 +0100

    BBB-408 Tidy up

--- src/acceptance-tests/resources/karate-config.js
+++ src/acceptance-tests/resources/karate-config.js
@@ -2,6 +2,6 @@ function() {
   return {
     baseUrl: 'http://localhost:8180/uk-gov-dft/user-management-service/1.0.0',
     managementBaseUrl: 'http://localhost:8181/manage',
-    authServerUrl: 'http://localhost:8380/bbb_auth'
+    authServerUrl: 'http://localhost:8380/bb_auth'
   }
 }
