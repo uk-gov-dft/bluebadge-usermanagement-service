e895d849352911ab2e446e1f7f6aedd45cf8db85
Stephen Elliott <stephen@bealine.co.uk>
2018-07-23 11:06:25 +0100

    BBB-586 Tidy up

--- src/main/java/uk/gov/dft/bluebadge/service/usermanagement/config/ActuatorSecurityConfig.java
+++ src/main/java/uk/gov/dft/bluebadge/service/usermanagement/config/ActuatorSecurityConfig.java
@@ -1,5 +1,7 @@
 package uk.gov.dft.bluebadge.service.usermanagement.config;
 
+import static uk.gov.dft.bluebadge.service.usermanagement.config.ActuatorSecurityConfig.BEFORE_RESOURCE_SERVER_ORDER;
+
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -7,8 +9,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Order(2) // Order before the resource server (which is 3)
+@Order(BEFORE_RESOURCE_SERVER_ORDER)
 public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+  /** Order before the resource server (which is 3) */
+  public static final int BEFORE_RESOURCE_SERVER_ORDER = 2;
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.requestMatcher(EndpointRequest.toAnyEndpoint())
