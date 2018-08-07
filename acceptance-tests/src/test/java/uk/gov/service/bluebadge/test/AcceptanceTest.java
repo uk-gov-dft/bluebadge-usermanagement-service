7beb9991b30f4b5a52aab280a0a5ffe8669f5fab
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-08-07 11:37:13 +0100

    Fix SonarQube issue of @SuppressWarnings("squid:S2187") on AcceptanceTest

--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/AcceptanceTest.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/AcceptanceTest.java
@@ -4,6 +4,7 @@ import com.intuit.karate.junit4.Karate;
 import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
+@SuppressWarnings("squid:S2187")
 @RunWith(Karate.class)
 @CucumberOptions(features = "classpath:features")
 public class AcceptanceTest {
