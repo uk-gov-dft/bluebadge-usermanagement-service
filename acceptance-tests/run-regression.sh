2263689f038227b6faedbebef838eb1316c0eb13
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-08-30 15:12:58 +0100

    Fixing the acceptance test setup.

--- acceptance-tests/run-regression.sh
+++ acceptance-tests/run-regression.sh
@@ -67,7 +67,7 @@ bash load-modules.sh
 docker-compose build
 docker-compose up -d --no-color
 ./wait_for_it.sh localhost:5432 localhost:8681:/manage/actuator/health localhost:8381:/manage/actuator/health localhost:8281:/manage/actuator/health localhost:8081:/manage/actuator/health localhost:8481:/manage/actuator/health localhost:8181:/manage/actuator/health localhost:8581:/manage/actuator/health
-#psql -h localhost -U developer -d bb_dev -f ./scripts/db/setup-user.sql
+psql -h localhost -U developer -d bb_dev -f ./scripts/db/setup-user.sql
 
 # Run the acceptance tests
 cd ..
