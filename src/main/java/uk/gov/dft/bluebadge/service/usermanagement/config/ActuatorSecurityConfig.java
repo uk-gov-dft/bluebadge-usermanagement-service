c792abd0a19bf11becb9a0c4771a1384b004a2ec
Paul Roberts <paul.roberts@valtech.com>
2019-04-03 16:37:17 +0100

    BBB-1320 log api version

--- src/main/java/uk/gov/dft/bluebadge/service/usermanagement/config/ActuatorSecurityConfig.java
+++ src/main/java/uk/gov/dft/bluebadge/service/usermanagement/config/ActuatorSecurityConfig.java
@@ -2,6 +2,7 @@ package uk.gov.dft.bluebadge.service.usermanagement.config;
 
 import static uk.gov.dft.bluebadge.service.usermanagement.config.ActuatorSecurityConfig.BEFORE_RESOURCE_SERVER_ORDER;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
@@ -12,16 +13,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import uk.gov.dft.bluebadge.common.actuator.MyBatisInfo;
 
-import javax.sql.DataSource;
-
 @Configuration
 @Order(BEFORE_RESOURCE_SERVER_ORDER)
 public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
   /** Order before the resource server (which is 3) */
   public static final int BEFORE_RESOURCE_SERVER_ORDER = 2;
 
-  @Autowired
-  DataSource dataSource;
+  @Autowired DataSource dataSource;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/uk/gov/dft/bluebadge/service/usermanagement/config/FilterConfig.java
+++ src/main/java/uk/gov/dft/bluebadge/service/usermanagement/config/FilterConfig.java
@@ -1,11 +1,16 @@
 package uk.gov.dft.bluebadge.service.usermanagement.config;
 
+import javax.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import uk.gov.dft.bluebadge.common.esapi.EsapiFilter;
 import uk.gov.dft.bluebadge.common.logging.JwtMdcFilter;
+import uk.gov.dft.bluebadge.common.logging.VersionFilter;
 
 @Configuration
+@PropertySource(value = "classpath:version.properties")
 public class FilterConfig {
 
   @Bean
@@ -14,6 +19,12 @@ public class FilterConfig {
   }
 
   @Bean
-  JwtMdcFilter getJwtMdcFilter() { return new JwtMdcFilter(); }
+  JwtMdcFilter getJwtMdcFilter() {
+    return new JwtMdcFilter();
+  }
 
+  @Bean
+  public VersionFilter getVersionFilter(@Value("${api.version}") @NotNull String apiVersion) {
+    return new VersionFilter(apiVersion);
+  }
 }
